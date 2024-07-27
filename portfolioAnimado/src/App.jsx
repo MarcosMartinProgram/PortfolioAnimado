5ea966bf6234d17d44f1ec77e834285e73bdd654
marcos <martinmarcospablo@gmail.com>
2024-07-27 17:39:59 -0300

    Services and Portfolio

--- portfolioAnimado/src/App.jsx
+++ portfolioAnimado/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
 import Parallax from "./components/parallax/Parallax";
 import Services from "./components/services/Services";
+import Portfolio from "./components/portfolio/Portfolio";
 
 function App() {
   
@@ -19,8 +20,7 @@ function App() {
         <section id="Portfolio"><Parallax type="portfolio"/></section>
         <section id="About">Portfiol1</section>
         <section id="Contact">Parallax</section>
-        <section>Portfolio3</section>
-        <section>Contact</section>
+        <Portfolio />
         
       </div>
       
---
+++ portfolioAnimado/src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,36 @@
+import "./portfolio.scss"
+
+const items = [
+    {
+        id:1,
+        title: "React e-commerce",
+        img: "" ,
+        desc: ""
+    },
+    {
+        id:1,
+        title: "React e-commerce",
+        img: "" ,
+        desc: ""
+    },
+    {
+        id:1,
+        title: "React e-commerce",
+        img: "" ,
+        desc: ""
+    },
+    {
+        id:1,
+        title: "React e-commerce",
+        img: "" ,
+        desc: ""
+    },
+]
+
+const Portfolio = () => {
+    return (
+        <div className="portfolio">Portfolio</div>
+    )
+}
+
+export default Portfolio
